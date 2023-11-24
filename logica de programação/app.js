5b7da890d62660f2a49744d972c50a3fe735382e
vinicius von <vini.r.faria@hotmail.com>
2023-11-24 11:13:47 -0300

    teste

---
+++ logica de programação/app.js
@@ -0,0 +1,14 @@
+alert(mensagemDeErro);
+//let numeroSecreto = 5;
+//let chute = prompt('Escolha um número entre 1 e 10');
+let nome = Lua;
+let idade = 25;
+let numeroDeVendas = 50;
+let saldoDisponivek = 1000;
+let mensagemDeErro = "Erro! Preencha todos os campos";
+
+
+
+/*if ( chute == numeroSecreto){
+    console.log('Isso ai! Você descobriu o número secreto (5)');
+}*/
