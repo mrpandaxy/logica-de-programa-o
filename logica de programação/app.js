da905089ea2b6a9a9a004b12991d5b1cf67e1a8c
vinicius von <vini.r.faria@hotmail.com>
2024-01-16 09:09:06 -0300

    finalizando o projeto

--- logica de programação/app.js
+++ logica de programação/app.js
@@ -1,14 +1,32 @@
-alert(mensagemDeErro);
-//let numeroSecreto = 5;
-//let chute = prompt('Escolha um número entre 1 e 10');
-let nome = Lua;
-let idade = 25;
-let numeroDeVendas = 50;
-let saldoDisponivek = 1000;
-let mensagemDeErro = "Erro! Preencha todos os campos";
+alert('Boas vindas ao jogo do número secreto');
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+// enquanto chute não for igual ao n.s.
+while (chute != numeroSecreto){
+    chute = prompt('Escolha um número entre 1 e 100');
+   // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`o número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++; 
+    }
+
+}
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
 
-
-/*if ( chute == numeroSecreto){
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+/*if (tentativas > 1){
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+}else{
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
 }*/
